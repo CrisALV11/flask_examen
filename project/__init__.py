cf0a2a9bc887c5ad4ec9a16a02f592bfa728716a
Cristian Almaguer <crisalmaguer2002@hotmail.com>
2023-03-18 18:30:09 -0600

    Inicio del proyecto

---
+++ project/__init__.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask
+from flask_security import Security, SQLAlchemyUserDatastore
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+from .models import User, Role
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+
+# Método de inicio de la aplicación
+def create_app():
+    # Creación de instancia de la clase Flask
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.urandom(24)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:1234@127.0.0.1:3306/flasksecurity'
+    app.config['SECURITY_PASSWORD_HASH'] = 'pbkdf2_sha512'
+    app.config['SECURITY_PASSWORD_SALT'] = 'thisissecretsalt'
+
+    db.init_app(app)
+
+    @app.before_first_request
+    def create_all():
+        db.create_all()
+
+    security = Security(app, user_datastore)
+
+    # Registro del blueprint para las rutas auth de la aplicación
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # Registro del blueprint para las rutas no auth de la aplicación
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+
+
+
---
+++ project/auth.py
@@ -0,0 +1,57 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_security import login_required
+from flask_security.utils import login_user, logout_user, hash_password, encrypt_password
+from .models import User
+from . import user_datastore, db
+
+auth = Blueprint('auth', __name__, url_prefix='/security')
+
+@auth.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+
+        # Verifica si hay un usuario creado con el email
+        user  = User.query.filter_by(email=email).first()
+
+        # Verifica si el usuario existe
+        if not user or not check_password_hash(user.password, password):
+            flash('El usuario y/o contraseña son incorrectos')
+            return redirect(url_for('auth.login'))
+
+        # Se autentica a el usuario
+        login_user(user, remember=remember)
+        return redirect(url_for('main.profile'))
+    return render_template('/security/login.html')
+
+@auth.route('/register',  methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+
+        # Verifica si el correo ya existe
+        if user:
+            flash('El correo electrónico ya está registrado')
+            return redirect(url_for('auth.signup'))
+
+        # Se crea un nuevo usuario con los datos del formulario
+        # Se realiza un hash a la contraseña
+        user_datastore.create_user(name=name, email=email, password=generate_password_hash(password, method='sha256'))
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+    return render_template('/security/register.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    # Cerrar sesión
+    logout_user()
+    return redirect(url_for('main.index'))
