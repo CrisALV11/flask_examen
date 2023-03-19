b70b9689402d8530404d9e451c1c2bedfeb1ea38
Cristian Almaguer <crisalmaguer2002@hotmail.com>
2023-03-19 00:15:03 -0600

    Cambios plantilla

--- project/main.py
+++ project/main.py
@@ -9,7 +9,7 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @main.route('/profile')
 @login_required
