4bffc488cbbc24b6bdffe7dda83e64289b61d7ba
Cristian Almaguer <crisalmaguer2002@hotmail.com>
2023-03-19 16:16:44 -0600

    Front chido

--- project/main.py
+++ project/main.py
@@ -11,38 +11,18 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+@main.route('/contact', methods=['POST', 'GET'])
+def contact():
+    if request.method == 'POST':
+        flash('¡Información enviada!')
+        return render_template('contact.html')
+    else:
+        return render_template('contact.html')
+
 @main.route('/profile')
 @login_required
 @roles_required('admin')
 def profile():
     return render_template('profile.html', name=current_user.name)
-
-@main.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        remember = True if request.form.get('remember') else False
-
-        # Verifica si hay un usuario creado con el email
-        user  = User.query.filter_by(email=email).first()
-
-        # Verifica si el usuario existe
-        if not user or not check_password_hash(user.password, password):
-            flash('El usuario y/o contraseña son incorrectos')
-            return redirect(url_for('main.login'))
-
-        # Se autentica a el usuario
-        login_user(user, remember=remember)
-        return redirect(url_for('main.profile'))
-    return render_template('/security/login.html')
-
-
-@main.route('/logout')
-@login_required
-def logout():
-    # Cerrar sesión
-    logout_user()
-    return redirect(url_for('main.index'))
-
+    
 
